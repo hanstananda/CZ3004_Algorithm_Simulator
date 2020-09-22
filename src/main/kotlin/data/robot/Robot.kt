e30d0337d91ec1071f11bb377e9c1f6ce0989bed
omh122 <ooiminhui122@gmail.com>
2020-09-22 23:27:14 +0800

    changed formatting

--- src/main/kotlin/data/robot/Robot.kt
+++ src/main/kotlin/data/robot/Robot.kt
@@ -39,7 +39,7 @@ data class Robot(var startRow: Int, var startCol: Int) : JPanel() {
     var delay: Int = RobotConstants.DELAY
     var row: Int = startRow
     var col: Int = startCol
-    var speed: Int = 1
+    var speed: Int = RobotConstants.DEF_SPEED
     private val logger = KotlinLogging.logger {}
 
     private val sensors: Array<Sensor> = arrayOf(
